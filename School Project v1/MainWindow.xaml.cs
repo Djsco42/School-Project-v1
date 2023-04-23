99343473531e8186fa325bb8009a219c4eb3be3d
Dylan Scofield <110282216+Djsco42@users.noreply.github.com>
2023-04-22 19:07:29 -0700

    ;(

--- School Project v1/MainWindow.xaml.cs
+++ School Project v1/MainWindow.xaml.cs
@@ -281,7 +281,7 @@ namespace School_Project_v1
                 }
             };
 
-            // Start the timer ree e
+            // Start the timer ree e e
             timer.Start();
         }
 
